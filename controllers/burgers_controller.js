7f320c89a5758200258693bae8e8d45bc0703d28
Christine Baba <baba.christine@gmail.com>
2016-11-03 23:57:59 -0500

    controller finished

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,36 @@
 
 var express = require('express');
 var router = express.Router();
-var cat = require('../models/burger.js');
+var burger = require('../models/burger.js');
+
+//get and posts
+router.get('/', function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = { burgers: data };
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/create', function (req, res) {
+    burger.create(['name', 'sleepy'], [req.body.name, req.body.sleepy], function () {
+        res.redirect('/');
+    });
+});
+
+router.put('/update/:id', function (req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    console.log('condition', condition);
+
+    burger.update({ sleepy: req.body.sleepy }, condition, function () {
+        res.redirect('/');
+    });
+
+});
+
+
+
 
 //export the `router`
 module.exports = router;
